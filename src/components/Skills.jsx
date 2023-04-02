bdc45ee7e42ca11c69344a20fd43c1f5629ccadc
Roig Antony Torres López <72164449+antonytorres21@users.noreply.github.com>
2023-04-01 18:33:17 -0600

    Agregando listas de imagenes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,138 @@ class Skills extends React.Component {
   render() {
     return (
       <div>
-        <h1>Skills</h1>
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div
+            id="controls-carousel"
+            class="relative w-full"
+            data-carousel="static"
+          >
+            <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+              <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img
+                  src="/docs/images/carousel/carousel-1.svg"
+                  class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                  alt="..."
+                />
+              </div>
+
+              <div
+                class="hidden duration-700 ease-in-out"
+                data-carousel-item="active"
+              >
+                <img
+                  src="/docs/images/carousel/carousel-2.svg"
+                  class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                  alt="..."
+                />
+              </div>
+
+              <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img
+                  src="/docs/images/carousel/carousel-3.svg"
+                  class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                  alt="..."
+                />
+              </div>
+
+              <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img
+                  src="/docs/images/carousel/carousel-4.svg"
+                  class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                  alt="..."
+                />
+              </div>
+
+              <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img
+                  src="/docs/images/carousel/carousel-5.svg"
+                  class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                  alt="..."
+                />
+              </div>
+            </div>
+
+            <button
+              type="button"
+              class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+              data-carousel-prev
+            >
+              <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg
+                  aria-hidden="true"
+                  class="w-6 h-6 text-white dark:text-gray-800"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 19l-7-7 7-7"
+                  ></path>
+                </svg>
+                <span class="sr-only">Previous</span>
+              </span>
+            </button>
+            <button
+              type="button"
+              class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+              data-carousel-next
+            >
+              <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg
+                  aria-hidden="true"
+                  class="w-6 h-6 text-white dark:text-gray-800"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 5l7 7-7 7"
+                  ></path>
+                </svg>
+                <span class="sr-only">Next</span>
+              </span>
+            </button>
+          </div>
+
+          <div class="p-5">
+            <a href="/">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <a
+              href="/"
+              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                aria-hidden="true"
+                class="w-4 h-4 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/list/ImgList.jsx
@@ -0,0 +1,80 @@
+import { Java } from "./scr/assets/logos/Java.png";
+import { AdobeXD } from "./scr/assets/logos/AdobeXD.png";
+import { Angular } from "./scr/assets/logos/Angular.png";
+import { asp } from "./scr/assets/logos/ASP.png";
+import { Cshart } from "./scr/assets/logos/Cshart.png";
+import { css } from "./scr/assets/logos/CSS.png";
+import { Dart } from "./scr/assets/logos/Dart.png";
+import { Figma } from "./scr/assets/logos/Figma.png";
+import { Firebase } from "./scr/assets/logos/Firebase.png";
+import { FireStore } from "./scr/assets/logos/FireStore.png";
+import { Flutter } from "./scr/assets/logos/Flutter.png";
+import { GitHub } from "./scr/assets/logos/GitHub.png";
+import { html } from "./scr/assets/logos/HTML.png";
+import { JavaScript } from "./scr/assets/logos/javaScript.png";
+import { Jira } from "./scr/assets/logos/Jira.png";
+import { Laravel } from "./scr/assets/logos/Laravel.png";
+import { MariaDB } from "./scr/assets/logos/Java.png";
+import { MaterialUi } from "./scr/assets/logos/MaterialUi.png";
+import { MySQL } from "./scr/assets/logos/MySQL.png";
+import { net } from "./scr/assets/logos/NEt.png";
+import { php } from "./scr/assets/logos/PHP.png";
+import { PhpMyAdmin } from "./scr/assets/logos/PhpMyAdmin.png";
+import { PostgreSQL } from "./scr/assets/logos/PostgreSQL.png";
+import { Python } from "./scr/assets/logos/Python.png";
+import { React } from "./scr/assets/logos/React.png";
+import { Redux } from "./scr/assets/logos/Redux.png";
+import { Scrum } from "./scr/assets/logos/Scrum.png";
+import { SQlite } from "./scr/assets/logos/SQlite.png";
+import { SQLserver } from "./scr/assets/logos/SQLserver.png";
+import { Tailwind } from "./scr/assets/logos/Tailwind.png";
+import { Typescript } from "./scr/assets/logos/Typescript.png";
+
+export const lengajes = [
+  { name: "Java", url: Java },
+  { name: "JavaScript", url: JavaScript },
+  { name: "Cshart", url: Cshart },
+  { name: "Dart", url: Dart },
+  { name: "PHP", url: php },
+];
+export const framewokrs = [
+  { name: "HTML", url: html },
+  { name: "CSS", url: css },
+  { name: "Flutter", url: Flutter },
+  { name: "React", url: React },
+  { name: "Laravel", url: Laravel },
+  { name: "Tailwind", url: Tailwind },
+  { name: "MaterialUi", url: MaterialUi },
+  { name: "Redux", url: Redux },
+];
+export const tools = [
+  { name: "Jira", url: Jira },
+  { name: "Figma", url: Figma },
+  { name: "AdobeXD", url: AdobeXD },
+  { name: "Scrum", url: Scrum },
+  { name: "GitHub", url: GitHub },
+  { name: "Firebase", url: Firebase },
+];
+
+export const databases = [
+  { name: "MySQL", url: MySQL },
+  { name: "PhpMyAdmin", url: PhpMyAdmin },
+  { name: "MariaDB", url: MariaDB },
+  { name: "PostgreSQL", url: PostgreSQL },
+  { name: "SQlite", url: SQlite },
+  { name: "SQLserver", url: SQLserver },
+  { name: "FireStore", url: FireStore },
+];
+
+export const learning = [
+  { name: "Angular", url: Angular },
+  { name: "React Native", url: React },
+  { name: "Python", url: Python },
+  { name: "Typescript", url: Typescript },
+];
+
+export const reinforcing = [
+  { name: ".NET", url: net },
+  { name: "ASP", url: asp },
+  { name: "React", url: React },
+];
